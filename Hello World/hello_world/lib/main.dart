836bff5edbbb261e3eb916ee98f3fe808b151692
Febriyadi <flutterlearning6@gmail.com>
2024-04-20 18:15:36 +0800

    first commit latihan widget

--- Hello World/hello_world/lib/main.dart
+++ Hello World/hello_world/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/clock_app_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ClockApp(title: 'Flutter Widget'),
     );
   }
 }
